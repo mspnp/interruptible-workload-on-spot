c1ea770c2c2707feddf43e46dd6b3a1d1137c190
Fernando Antivero <v-feanti@microsoft.com>
2022-05-27 23:40:50 +0000

    add queue receive/delete msgs logic

--- src/Worker.cs
+++ src/Worker.cs
@@ -1,7 +1,12 @@
 namespace interruptible_workload;
 
+using Azure.Identity;
+using Azure.Storage.Queues;
+
 public class Worker : BackgroundService
 {
+    private readonly static string s_storageAccountName = "";
+    private readonly static string s_queueName = "";
     private readonly ILogger<Worker> _logger;
 
     public Worker(ILogger<Worker> logger)
@@ -15,6 +20,19 @@ public class Worker : BackgroundService
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(1000, stoppingToken);
+
+            var queueClient = new QueueClient(
+              new Uri($"https://{s_storageAccountName}.queue.core.windows.net/{s_queueName}"),
+              new DefaultAzureCredential());
+
+            foreach (var message in (await queueClient.ReceiveMessagesAsync(maxMessages: 10)).Value)
+            {
+                Console.WriteLine($"Message: {message.Body}");
+
+                await queueClient.DeleteMessageAsync(
+                  message.MessageId,
+                  message.PopReceipt);
+            }
         }
     }
 }
