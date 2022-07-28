607e6832e01e88aa32654f7476c6959dcc52169e
Fernando Antivero <v-feanti@microsoft.com>
2022-07-28 21:50:04 +0000

    add some guidance and code comments

--- src/Worker.cs
+++ src/Worker.cs
@@ -17,13 +17,26 @@ public class Worker : BackgroundService
 
     public override async Task StartAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("* -> [recover] -> [resume] ...");
+        // Recover
+        // In this architecture it is reached right after the VM is started for
+        // the first time or after eviction (redeployed or restarted).
+        // No other services are getting started until this run to completion 
+        // and it is critical to start querying the Azure Event Scheduled as soon as 
+        // possible (remember the up to 30 sec notice) taking into consideration that
+        // a Spot Virtual Machine can be evicted right after it has been started.
+        // Therefore, recovering should execute limited to short running tasks.
+        _logger.LogInformation("* -> [recover] ...");
         await base.StartAsync(cancellationToken);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        // Resume
+        // After this point the appliation must be ready to start processing 
+        // messages after doing a best effort to recover from a previous checkpoint
         _logger.LogInformation("* -> [recover] -> [resume] -> [start] -> ...");
+
+        // Start processing
         while (!stoppingToken.IsCancellationRequested)
         {
             try
@@ -52,6 +65,16 @@ public class Worker : BackgroundService
 
     public override async Task StopAsync(CancellationToken cancellationToken)
     {
+        // Shutdown
+        // An eviction notice been detected. For more information about how to
+        // implement this dectection, please take a look at the ScheduledEvents 
+        // background service.
+        // At this point Azure infrastructure is claiming for this Spot VM instance.
+        // In this reference implementation, you are querying every 1 sec 
+        // the Azure Scheduled Event metadata endpoint, and eviction notices
+        // are expected to be scheduled with up to up to 30 seconds in advance. 
+        // Ensure you quicly stop the application within a reasonable amount 
+        // of time to prevent from being forcedly shutdown.  
         _logger.LogInformation("* -> [recover] -> [resume] -> [start] -> [shutdown] -> *");
         await base.StopAsync(cancellationToken);
         _logger.LogInformation("gracefull shutdown OK...");
