e94166afe5056a6c4740842a3371f724ae93837b
Fernando Antivero <v-feanti@microsoft.com>
2022-08-10 16:43:30 +0000

    Address PR Feedback: remove unused telemetry config

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,6 @@
 using Azure.Identity;
 using Azure.Storage;
 using Azure.Storage.Queues;
-using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.ApplicationInsights.Extensibility.Implementation;
-using Azure.Storage;
 using Microsoft.Extensions.Azure;
 using interruptible_workload;
 
@@ -11,20 +8,6 @@ IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
         services.AddHostedService<Worker>();
-        // configure telemetry
-        services.Configure<TelemetryConfiguration>(
-          config => {
-            if (hostContext.HostingEnvironment.IsDevelopment())
-            {
-              TelemetryDebugWriter.IsTracingDisabled = false;
-              config.DisableTelemetry = false;
-            }
-            else
-            {
-              TelemetryDebugWriter.IsTracingDisabled = false;
-              config.DisableTelemetry = false;
-            }
-          });
 
         services.AddApplicationInsightsTelemetryWorkerService();
 
